77f28152f3344fa1707ab4debc77470debcc2adf
sungbbb <ksbea123@gmail.com>
2024-09-02 02:17:52 +0900

    feat: 인증 아이콘 수정

--- src/Assets/icons.tsx
+++ src/Assets/icons.tsx
@@ -1,4 +1,14 @@
-import { Center, chakra, type HTMLChakraProps } from "@chakra-ui/react";
+import { Center, chakra, type HTMLChakraProps, Image} from "@chakra-ui/react";
+import samsungPassIcon from "../Assets/Icon/samsungpass.webp";
+import tossIcon from "../Assets/Icon/toss.webp";
+
+export const SamsungPassIcon = (props: HTMLChakraProps<"img">) => (
+  <Image src={samsungPassIcon} alt="삼성패스" {...props} />
+);
+
+export const TossIcon = (props: HTMLChakraProps<"img">) => (
+  <Image src={tossIcon} alt="토스" {...props} />
+);
 
 export const KakaoIcon = (props: HTMLChakraProps<"svg">) => (
   <chakra.svg
@@ -14,7 +24,7 @@ export const KakaoIcon = (props: HTMLChakraProps<"svg">) => (
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <g clip-path="url(#clip0_1615_2)">
+      <g clipPath="url(#clip0_1615_2)">
         <path
           d="M42.873 46.5H5.127C3.129 46.5 1.5 44.871 1.5 42.873V5.127C1.5 3.129 3.129 1.5 5.127 1.5H42.873C44.871 1.5 46.5 3.129 46.5 5.127V42.873C46.5 44.871 44.871 46.5 42.873 46.5Z"
           fill="#FAE300"
@@ -67,9 +77,9 @@ export const PassIcon = (props: HTMLChakraProps<"svg">) => (
       <g
         id="logo/pass"
         stroke="none"
-        stroke-width="1"
+        strokeWidth="1"
         fill="none"
-        fill-rule="evenodd"
+        fillRule="evenodd"
       >
         <g id="Group-7">
           <rect
@@ -85,7 +95,7 @@ export const PassIcon = (props: HTMLChakraProps<"svg">) => (
             id="Layer_1_1_"
             transform="translate(0.000000, 8.000000)"
             fill="#222222"
-            fill-rule="nonzero"
+            fillRule="nonzero"
           >
             <g id="Group">
               <path
@@ -122,9 +132,9 @@ export const NaverIcon = (props: HTMLChakraProps<"svg">) => (
       <g
         id="logo/naver"
         stroke="none"
-        stroke-width="1"
+        strokeWidth="1"
         fill="none"
-        fill-rule="evenodd"
+        fillRule="evenodd"
       >
         <g id="Group-2">
           <rect
@@ -165,9 +175,9 @@ export const PaycoIcon = (props: HTMLChakraProps<"svg">) => (
       <g
         id="logo/payco"
         stroke="none"
-        stroke-width="1"
+        strokeWidth="1"
         fill="none"
-        fill-rule="evenodd"
+        fillRule="evenodd"
       >
         <g id="Group-6">
           <rect
@@ -183,7 +193,7 @@ export const PaycoIcon = (props: HTMLChakraProps<"svg">) => (
             d="M6,5.024 C6,3.344 7.29562982,2 8.9151671,2 L16.8508997,2 C23.1670951,2 27,5.864 27,11.52 L27,11.576 C27,17.96 22.2493573,21.264 16.2570694,21.264 L11.8303342,21.264 L11.8303342,26.976 C11.8303342,28.656 10.5347044,30 8.9151671,30 C7.29562982,30 6,28.656 6,26.976 L6,5.024 Z M16.471345,15.944 C19.5660819,15.944 21.4,14.152 21.4,11.8 L21.4,11.744 C21.4,9.056 19.451462,7.6 16.3567251,7.6 L11.6,7.6 L11.6,16 L16.471345,16 L16.471345,15.944 Z"
             id="Shape"
             fill="#E7241F"
-            fill-rule="nonzero"
+            fillRule="nonzero"
           ></path>
         </g>
       </g>
@@ -209,9 +219,9 @@ export const KBIcon = (props: HTMLChakraProps<"svg">) => (
       <g
         id="logo/kb"
         stroke="none"
-        stroke-width="1"
+        strokeWidth="1"
         fill="none"
-        fill-rule="evenodd"
+        fillRule="evenodd"
       >
         <g id="Group-4">
           <rect
@@ -226,7 +236,7 @@ export const KBIcon = (props: HTMLChakraProps<"svg">) => (
           <g
             id="Group"
             transform="translate(5.000000, 0.000000)"
-            fill-rule="nonzero"
+            fillRule="nonzero"
           >
             <path
               d="M17.3333333,9.33333333 L15.5392616,9.33333333 L15.5392616,6.29263158 C15.5392616,3.75789474 13.3551742,1.69473684 10.6666667,1.69473684 C7.97815913,1.69473684 5.79407176,3.75789474 5.79407176,6.29263158 L5.79407176,9.33333333 L4,9.33333333 L4,6.29263158 C4,2.8245614 6.9901196,0 10.6614665,0 C14.3328133,0 17.3229329,2.8245614 17.3229329,6.29263158 L17.3229329,9.33333333 L17.3333333,9.33333333 Z"
@@ -272,7 +282,7 @@ export const SinhanIcon = (props: HTMLChakraProps<"svg">) => (
       version="1.1"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <g id="pc" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+      <g id="pc" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
         <g id="image" transform="translate(-274.000000, -335.000000)">
           <rect fill="#FFFFFF" x="0" y="0" width="446" height="684"></rect>
           <g id="logo/hana" transform="translate(274.000000, 335.000000)">
@@ -300,51 +310,4 @@ export const SinhanIcon = (props: HTMLChakraProps<"svg">) => (
     </svg>
   </chakra.svg>
 );
-export const TossIcon = (props: HTMLChakraProps<"svg">) => (
-  <chakra.svg
-    color="accent"
-    viewBox="0 0 48 48"
-    xmlns="http://www.w3.org/2000/svg"
-    {...props}
-  >
-    <svg
-      width="48"
-      height="48"
-      viewBox="0 0 48 48"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <g clip-path="url(#clip0_1615_2)">
-        <path
-          d="M42.873 46.5H5.127C3.129 46.5 1.5 44.871 1.5 42.873V5.127C1.5 3.129 3.129 1.5 5.127 1.5H42.873C44.871 1.5 46.5 3.129 46.5 5.127V42.873C46.5 44.871 44.871 46.5 42.873 46.5Z"
-          fill="#FAE300"
-        />
-        <path
-          d="M23.9956 9C14.6774 9 7.125 15.1381 7.125 22.7141C7.125 27.6479 10.3243 31.9616 15.132 34.3791C14.7823 35.7361 13.8558 39.286 13.6722 40.0499C13.4449 40.9935 14.0131 40.9845 14.3802 40.7239C14.6687 40.5262 19.0044 37.4976 20.875 36.1855C21.889 36.3383 22.9379 36.4191 24.0044 36.4191C33.3226 36.4191 40.875 30.2811 40.875 22.7051C40.875 15.1381 33.3138 9 23.9956 9Z"
-          fill="#391B1B"
-        />
-        <path
-          d="M16.9362 18.5645H11.7865C11.3034 18.5645 10.9115 18.9717 10.9115 19.4735C10.9115 19.9754 11.3034 20.3826 11.7865 20.3826H13.4636V26.7842C13.4636 27.0209 13.5547 27.2482 13.7188 27.4092C13.8737 27.5701 14.0925 27.6648 14.3204 27.6648H14.5118C14.7396 27.6648 14.9584 27.5701 15.1133 27.4092C15.2774 27.2387 15.3685 27.0114 15.3685 26.7842V20.3826H16.9454C17.4284 20.3826 17.8204 19.9754 17.8204 19.4735C17.8112 18.9717 17.4193 18.5645 16.9362 18.5645Z"
-          fill="#FAE300"
-        />
-        <path
-          d="M29.2956 25.9693H26.9076V19.4258C26.9076 18.8765 26.4792 18.4409 25.9596 18.4409C25.431 18.4409 25.0117 18.886 25.0117 19.4258V26.4712C25.0117 26.528 25.0208 26.5754 25.0299 26.6322C25.0208 26.6796 25.0117 26.7364 25.0117 26.7932C25.0117 27.2477 25.3672 27.6171 25.8047 27.6171H29.3047C29.7422 27.6171 30.0977 27.2477 30.0977 26.7932C30.0885 26.3386 29.7331 25.9693 29.2956 25.9693Z"
-          fill="#FAE300"
-        />
-        <path
-          d="M36.9427 26.2629L34.2721 22.6265L36.6784 20.1265C37.0065 19.7856 37.0065 19.2364 36.6784 18.8955C36.3503 18.5546 35.8216 18.5546 35.4935 18.8955L32.513 21.9921V19.4258C32.513 18.8765 32.0846 18.4409 31.5651 18.4409C31.0365 18.4409 30.6172 18.886 30.6172 19.4258V26.689C30.6172 27.2383 31.0456 27.6739 31.5651 27.6739C32.0938 27.6739 32.513 27.2288 32.513 26.689V24.4447L33.0508 23.886L35.5755 27.3235C35.8581 27.7118 36.3867 27.797 36.7604 27.5034C37.1523 27.2099 37.2253 26.6606 36.9427 26.2629Z"
-          fill="#FAE300"
-        />
-        <path
-          d="M24.2918 26.4998L21.7306 19.2082L21.7215 19.1987C21.5574 18.7536 21.0834 18.4316 20.5183 18.4316C19.8894 18.4316 19.379 18.8294 19.2696 19.3502C19.2514 19.3881 19.2332 19.426 19.2241 19.4733L16.7084 26.4998C16.5535 26.9638 16.7905 27.4657 17.228 27.6267C17.6746 27.7877 18.1576 27.5415 18.3126 27.0869L18.7501 25.7896H22.2592L22.6967 27.0869C22.8517 27.551 23.3347 27.7877 23.7813 27.6267C24.2188 27.4657 24.4467 26.9638 24.2918 26.4998ZM19.3061 24.1419L20.4819 20.6475C20.491 20.6475 20.5092 20.6475 20.5183 20.6475L21.6941 24.1419H19.3061Z"
-          fill="#FAE300"
-        />
-      </g>
-      <defs>
-        <clipPath id="clip0_1615_2">
-          <rect width="48" height="48" fill="white" />
-        </clipPath>
-      </defs>
-    </svg>
-  </chakra.svg>
-);
+
